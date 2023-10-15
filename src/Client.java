db8e8bf346c67a8c6075107151d95c71bfac0c57
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-15 13:28:37 +0200

    This class will represent a client in the network.

--- src/Client.java
+++ src/Client.java
@@ -1,22 +1,19 @@
 // This class will represent a client in the network.
+
+@SuppressWarnings({ "unchecked", "rawtypes", "generics" })
 public class Client {
-    private String name;
-    private OptimisticListSynchronization<Message> messages;
-    private Server server;
+    private OptimisticListSynchronization messages;
     private TTASLock lock;
 
-    public Client(String name, Server server) {
-        this.name = name;
-        this.server = server;
+    public Client() {
         this.messages = new OptimisticListSynchronization<>();
         this.lock = new TTASLock();
     }
 
-    public void write(Message message) {
+    public void write(String message, String recipient, String sender) {
         lock.lock();
         try {
-            messages.add(message);
-            System.out.println("(SEND) " + Thread.currentThread().getName() + ": SUCCESSFUL");
+            while (!messages.add(Thread.currentThread(), message, recipient, sender)){};
         } finally {
             lock.unlock();
         }
@@ -25,39 +22,9 @@ public class Client {
     public void read() {
         lock.lock();
         try {
-            Message message = server.getMessage(this.name);
-            if (message != null) {
-                System.out.println("(RECEIVE) " + Thread.currentThread().getName() + ": " + message.toString());
-            }
+            while (!messages.remove(Thread.currentThread())){};
         } finally {
             lock.unlock();
         }
-    }
-
-    public void start() {
-        Thread writerThread = new Thread(() -> {
-            while (true) {
-                // Generate a random message and recipient
-                Message message = new Message();
-                // ... initialize the message ...
-
-                write(message);
-
-                // Sleep for a while
-                try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
-            }
-        });
-
-        Thread readerThread = new Thread(() -> {
-            while (true) {
-                read();
-
-                // Sleep for a while
-                try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
-            }
-        });
-
-        writerThread.start();
-        readerThread.start();
     }
 }
