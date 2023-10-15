acde17bfcd1f0f1b2bb319324ec9f38da56393cb
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-15 19:57:58 +0200

    The Client class for Rending and Reading Messages

--- test/Client.java
+++ test/Client.java
@@ -3,32 +3,19 @@
 @SuppressWarnings({ "unchecked", "rawtypes", "generics" })
 public class Client {
     private MessageSynchronization messages;
-    private TTASLock lock;
     String clientName;
 
     public Client(String clientName) {
         this.clientName = clientName;
         this.messages = new MessageSynchronization<>();
-        this.lock = new TTASLock();
     }
 
-    public void write(String message, String recipient, String sender) {
-        System.out.println("(SEND) [" + Thread.currentThread().getName() + "]: { sender:[" + sender + "] , recipient:[" + recipient + "]}");
-        lock.lock();
-        try {
-            messages.write(Thread.currentThread(), message, recipient, sender);
-        } finally {
-            lock.unlock();
-        }
+    public void write(String message, String recipient) {
+        messages.write(Thread.currentThread(), message, recipient, this.clientName);
     }
 
-    public void read(String recipient) {
-        lock.lock();
-        try {
-            messages.read(recipient);
-        } finally {
-            lock.unlock();
-        }
+    public void read() {
+        messages.read(this.clientName);
     }
 
     public String getClientName() {
