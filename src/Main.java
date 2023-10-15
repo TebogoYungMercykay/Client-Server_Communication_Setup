679c0d07bad8363c2e57cb5b13a80cd044f47c65
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-15 16:49:02 +0200

    Updated the Testing Main for the Server.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
-// - *Main*: This class will represent the Main.
+// - *Main*: This is the Testing Main for the Server.
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class Main {
     public static void main(String[] args) {
-        Client[] clients = new Client[5]; // Create 5 clients
+        // Creating 5 Initial Clients On the Server
+        Client[] clients = new Client[5];
         for (int i = 0; i < clients.length; i++) {
             clients[i] = new Client();
         }
 
-        Server ClientServer = new Server(clients);
+        // The Number of messages Each Client Shall Send to Other Clients
+        int numMessages = 5;
+        Server ClientServer = new Server(clients, numMessages);
         ClientServer.start();
     }
 }
