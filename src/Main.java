29a6da30f786c8652db0c551233b700f56ce967d
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-15 18:43:06 +0200

    Created 5 Clients to Send at Least 10 Messages

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,27 @@
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class Main {
     public static void main(String[] args) {
+        // The Current Number of Clients
+        int numClients = 5;
+
+        // The Current Number of Messages each Client Sends
+        int numMessages = 10;
+
+        // Creating an Array of Client Names
+        String[] serverClients = new String[numClients];
+        serverClients[0] = "Thabo";
+        serverClients[1] = "Ntando";
+        serverClients[2] = "Luke";
+        serverClients[3] = "Scott";
+        serverClients[4] = "Michael";
+
         // Creating 5 Initial Clients On the Server
-        Client[] clients = new Client[5];
-        for (int i = 0; i < clients.length; i++) {
-            clients[i] = new Client();
+        Client[] clients = new Client[numClients];
+        for (int i = 0; i < numClients; i++) {
+            clients[i] = new Client(serverClients[i]);
         }
 
-        // The Number of messages Each Client Shall Send to Other Clients
-        int numMessages = 5;
-        Server ClientServer = new Server(clients, numMessages);
-        ClientServer.start();
+        Server serverObject = new Server(clients, serverClients, numMessages);
+        serverObject.start();
     }
 }
