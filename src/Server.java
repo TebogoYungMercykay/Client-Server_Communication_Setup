6b2e4ae7759ff3942c1642994b3aa65784de1022
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-15 12:48:44 +0200

    This class will represent the server

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,31 @@
+// - *Server*: This class will represent the server.
+import java.util.Map;
+import java.util.HashMap;
+
 public class Server {
-    // Code Here
+    private FineGrainList<Map<String, Client>> clientMessages;
+    private AndersonLock lock;
+
+    public Server() {
+        this.clientMessages = new FineGrainList(new HashMap<>());
+        this.lock = new AndersonLock();
+    }
+
+    public void setMessage(String clientName, Message message) {
+        lock.lock();
+        try {
+            clientMessages.getUnderlying().put(clientName, message);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Message getMessage(String clientName) {
+        lock.lock();
+        try {
+            return clientMessages.getUnderlying().get(clientName);
+        } finally {
+            lock.unlock();
+        }
+    }
 }
