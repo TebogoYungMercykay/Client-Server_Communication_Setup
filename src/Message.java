6455ff8ddfbcba54e1a11f4ddd8b08201025a6c7
TebogoYungMercykay <sbkskhalo.kq@gmail.com>
2023-10-15 10:43:05 +0200

    Class: Will Hold the Chat Messages in the Server

---
+++ src/Message.java
@@ -0,0 +1,81 @@
+import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+// - *Message*: This class will represent a message sent or received by a client. It should have properties like `sender`, `recipient`, and `content`. It should also override the `toString()`, `equals()`, and `hashCode()` methods.
+
+// java
+
+public class Message {
+    private String sender;
+    private String recipient;
+    private String content;
+    public int key;
+    public Lock lock = new ReentrantLock();
+    String threadName;
+
+    public Message(String message, String recipient, String sender) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = message;
+        this.threadName = Thread.currentThread().getName();
+        this.key = hashCode();
+    }
+
+    public Message() {
+        this.sender = "Jane Doe";
+        this.recipient = "John Doe";
+        this.content = "Hey There Hero, You've Been Out A Long Time!!";
+        this.key = hashCode();
+        this.threadName = Thread.currentThread().getName();
+    }
+
+    @Override
+    public String toString() {
+        return "Message: { sender: " + sender + ", recipient: " + recipient + ", content: " + content + " }";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        // Casting
+        Message message = (Message) obj;
+        return Objects.equals(sender, message.sender) && Objects.equals(recipient, message.recipient) && Objects.equals(content, message.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sender, recipient, content);
+    }
+
+    // Getters and Setters
+
+    public String getSender() {
+        return this.sender;
+    }
+
+    public String getRecipient() {
+        return this.recipient;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
